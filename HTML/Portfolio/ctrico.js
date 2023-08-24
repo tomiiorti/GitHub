6a448f72e30a4da14cc6b73d513a4155d36f515e
Tomas Orti <tomas.orti99@gmail.com>
2023-08-24 18:13:46 -0300

    Portafolio

---
+++ HTML/Portfolio/ctrico.js
@@ -0,0 +1,28 @@
+window.requestAnimFrame = function (){
+    return(
+        window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRquestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function (callback){
+            window.setTimeout(callback);
+        }
+    );
+};
+
+function init(elmid){
+    let canvas =document.getElementById(elemid),
+    c = canvas.getContext("2d"),
+    w = (canvas.width = window.innerWidth),
+    h = (canvas.height = windows.innerHeight);
+    c.fillStyle = "rgba(30,30,30,1)";
+    c.fillRect(0,0,w,h);
+    return { c: c, canvas: canvas};
+}
+window.onloead = function(){
+    let c = init("canvas").c,
+    canvas = init("canvas").canvas,
+    w = (canvas.width = window.innerWidth),
+    h = (canvas.height = window.innerHeight)
+}
