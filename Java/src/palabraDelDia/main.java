fb0a727a6564f281583e0a70c4947d7c4ea1a3bf
Tomas Orti <tomas.orti99@gmail.com>
2023-10-03 19:05:42 -0300

    java

--- Java/src/palabraDelDia/main.java
+++ Java/src/palabraDelDia/main.java
@@ -7,32 +7,62 @@ public class main {
 
     public static void main(String[] args) {
     	
-    	Scanner sc = new Scanner (System.in);
-    	
-    	
-        FiltrarPalabras nuevo = new FiltrarPalabras();
-        nuevo.filtro();
-        
-        //Leer5Palabras leyendo = new Leer5Palabras();
-        //leyendo.leer();
-        
-        
-    	for (int i=0; i<=6; i++) {
-    		
-    		System.out.println("Ingrese una palabra de 5 letras");
-        	String entrada = sc.next();
-    		
-	        while (entrada.length() != 5) {
-	        	
-	        	System.out.println("Tiene que ingresar una palabra con 5 letras nada mas");
-	        	entrada = sc.next();
-	        }
-	        
-	        
-	        
+    	Random random = new Random();
+        Scanner sc = new Scanner(System.in);
+        FileReader palabra = null;
+        String palabraDelDia = null; // Declaración dentro del bloque try
+        List<String> listaDePalabras = new ArrayList<>();
+
+        try {
+            palabra = new FileReader("C:/Users/tomas/OneDrive/Escritorio/GitHub/Java/src/palabraDelDia/destino.txt");
+            BufferedReader buf = new BufferedReader(palabra);
+            String linea;
+            
+
+            while ((linea = buf.readLine()) != null) {
+                listaDePalabras.add(linea);
+            }
+
+            palabraDelDia = listaDePalabras.get(random.nextInt(listaDePalabras.size()));
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 5; i >=1; i--) {
+            System.out.println("Ingrese una palabra de 5 letras");
+            System.out.println("Tiene " + i + " intentos");
+            String entrada = sc.next();
+            
+            while (!listaDePalabras.contains(entrada)) {
+            	System.out.println("La palabra ingresada no esta en el texto \n Ingrese nuevamente una palabra");
+            	entrada = sc.next();	
+            }
+            /*while (entrada !=  ) {
+            	
+            	if (listaDePalabras.contains(entrada)) {
+                    System.out.println("¡La palabra está en la lista!");
+                    break; // Sale del bucle si la palabra es correcta
+                } else {
+                    System.out.println("Esa palabra no está en la lista. Inténtalo de nuevo.");
+                }
+            }
+            
+            if (palabraDelDia != null && palabraDelDia.equals(entrada)) {
+                System.out.println("¡Adivinaste la palabra del día!");
+                break; // Sale del bucle si la palabra es correcta
+            } else {
+                System.out.println("Esa no es la palabra del día. Inténtalo de nuevo.");
+            }*/
+        }
+    }
+}
+	        /*
+	      
 	        FileReader palabras;
-			try {
-				
+			try {		
 				palabras = new FileReader("C:/Users/tomas/OneDrive/Escritorio/GitHub/Java/src/palabraDelDia/destino.txt");
 				BufferedReader mibuffer = new BufferedReader(palabras);
 		        String linea;
@@ -57,9 +87,10 @@ public class main {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-    	}
-    }
-}
+			*/
+
+
+	        
 
 class FiltrarPalabras {
 
