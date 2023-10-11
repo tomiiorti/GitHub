d5042e929b1b850f109dd16890bf431a8633ecb3
Tomas Orti <tomas.orti99@gmail.com>
2023-10-11 11:13:33 -0300

    Modal

--- HTML/landingpage copy/script.js
+++ HTML/landingpage copy/script.js
@@ -14,3 +14,18 @@ var swiper = new Swiper (".mySwiper",  {
         }
     }
 });
+
+const btnAbrirModal = 
+document.querySelector("#btn-abrir-modal");
+const btnCerrarModal =
+document.querySelector("#btn-cerrar-modal");
+const modal =
+document-querySelector("#modal");
+
+btnAbrirModal.addEventListener("click",() =>{
+    modal.showModal();
+})
+
+btnCerrarModal.addEventListener("click", () =>{
+    modal.close();
+})
