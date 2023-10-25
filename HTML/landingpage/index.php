45ff09328a2a39130ebb22abb75c33122cdcfff3
Tomas Orti <tomas.orti99@gmail.com>
2023-10-25 17:26:56 -0300

    cliente

---
+++ HTML/landingpage/index.php
@@ -0,0 +1,9 @@
+<?php
+$nombre = $_REQUEST['nombre'];
+$apellido = $_REQUEST['apellido'];
+$usuario = $_REQUEST['usuario'];
+$contraseña = $_REQUEST['contraseña'];
+$id = $_REQUEST['id'];
+echo "Hola $nombre $apellido, como estas? tu contraseña es '$contraseña'";
+
+?>
